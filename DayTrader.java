7ef4d0e1a9ad0d593e8fbbb56b150c2cbeff06a7
EvgeniUschakov <evgeni.uschakov@gmail.com>
2020-01-13 15:16:01 +0100

    Add files via upload

---
+++ DayTrader.java
@@ -0,0 +1,15 @@
+public class DayTrader {
+
+    public DayTrader(double p) {
+	price = p;
+    }
+
+    // setter ----------------------------------------------
+    public void setPrice(double p) {
+	price = p;
+    }
+
+    // private ---------------------------------------------
+    private double price;
+
+}
